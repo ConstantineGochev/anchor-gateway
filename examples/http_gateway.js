c9940b2db601898ac9bc59d430f72f48bdf284ff
constantin <constantin@anchor.chain.com>
2020-03-01 18:49:29 +0200

    asd

--- examples/http_gateway.js
+++ examples/http_gateway.js
@@ -2,13 +2,19 @@ const AnchorGateway = require('../index.js')
 
 const gateway = new AnchorGateway({
   port: 8000,
-  xfwd: false // OPTIONAL: Setup your gateway but disable the X-Forwarded-For header
+  xfwd: false,
+  redis: {} // OPTIONAL: Setup your gateway but disable the X-Forwarded-For header
 })
 
 gateway.init([{
   src: 'localhost/admin',
-  target: 'http://127.0.0.1:3001'
+  target: 'http://127.0.0.1:3001',
+  cache: ['/some/test/4', '/some/test/5']
 }, {
   src: 'localhost/boats',
-  target: 'http://127.0.0.1:3003'
+  target: 'http://127.0.0.1:3003',
+
 }])
+gateway.get_all_services().then(function(services) {
+  console.log(services)
+}).catch(err => console.log(err))
